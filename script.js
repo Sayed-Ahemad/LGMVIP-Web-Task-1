9193c80c04376058865aba3b7cbd0b19727e1aac
Sayed <sayedahemad47@gmail.com>
2021-11-30 12:12:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello, Welcome to Foodiezz");
+const links = document.querySelectorAll("#navbar ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behaviour: "smooth"
+  });
+}
